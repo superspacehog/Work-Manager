947262a4301dee9109c6939c0db4c91858ab56a8
superspacehog <117591514+superspacehog@users.noreply.github.com>
2024-10-24 17:55:16 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route("/") 
+def servePage(): 
+	return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+if __name__ == '__main__':
+    socketio.run(app)
